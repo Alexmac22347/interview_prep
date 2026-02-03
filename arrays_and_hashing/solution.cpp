60645815a7b6b0e31165f864fc1b984907cde9dc
aikm94 <aikm94@ymail.ne.jp>
2026-02-03 14:23:05 +0900

    encoder: trouble with edge cases. basic cases are working, but empty array, and array with empty string are causing problems. current implementation has no way to distinguish

--- arrays_and_hashing/solution.cpp
+++ arrays_and_hashing/solution.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <vector>
 #include <iostream>
 #include <string>
@@ -9,16 +10,19 @@
 #include "solution.h"
 
 void Solution::runTests() {
-    std::vector<std::string> anagrams = {"act", "cat", "tops", "stop", "hat", "cat", "pots"};
+    //std::vector<std::string> message{""};
+    //std::vector<std::string> message{",,"};
+    //std::vector<std::string> message{"hi"};
+    std::vector<std::string> message{",test","message,",",,,,"};
+    //std::vector<std::string> message{"mge,",",,,,"};
 
-    auto a = groupAnagrams(anagrams);
+    std::string encoded = encode(message);
+    std::cout << "encoded: " << encoded << std::endl;
 
-    for (auto anagram_list : a) {
-        std::cout << "group:" << std::endl;
-        for (auto s : anagram_list) {
-            std::cout << s << ", ";
-        }
-        std::cout << std::endl;
+    std::vector<std::string> decoded = decode(encoded);
+    std::cout << "decoded message:" << std::endl;
+    for (std::string& s : decoded) {
+        std::cout << s << std::endl;
     }
 }
 
@@ -84,6 +88,98 @@ std::vector<
 
     for (auto it : anagrams) {
         retval.push_back(it.second);
+    }
+
+    return retval;
+}
+
+std::vector<int> Solution::topKFrequent(std::vector<int>& nums, int k) {
+    std::unordered_map<int, int> counts;
+
+    for (int& num : nums) {
+        if (counts.find(num) == counts.end()) {
+            counts[num] = 1;
+        } else {
+            counts[num]++;
+        }
+    }
+
+    // i hate sorting and i hate dragging in more
+    // complex datatypes. to find the largest k counts:
+    // find the max that is not in retval, and add it to retval,
+    // until retval.length() == k
+    std::vector<int> retval;
+    for (int i = 0; i < k; i++) {
+        int max = 0;
+        int max_key = 0;
+        for (auto& count : counts) {
+            if (count.second > max) {
+                max = count.second;
+                max_key = count.first;
+            }
+        }
+
+        retval.push_back(max_key);
+        counts.erase(max_key);
+    }
+
+    return retval;
+}
+
+std::string Solution::encode(std::vector<std::string>& strs) {
+    if (strs.size() == 0) {
+        return "";
+    }
+    // use ENCODE_SEPARATOR to separate strings.
+    // if ENCODE_SEPARATOR appears in strs, prepend
+    // escape character 
+    std::ostringstream ss;
+    for (unsigned int i = 0; i < strs.size(); i++) {
+        for (char c : strs[i]) {
+            if (c == ENCODE_SEPARATOR) {
+                ss << ENCODE_ESCAPE;
+            }
+            ss << c;
+        }
+
+        // do not add a comma after the last string
+        if (i != strs.size()-1) {
+            ss << ENCODE_SEPARATOR;
+        }
+    }
+
+    return ss.str();
+}
+
+std::vector<std::string> Solution::decode(std::string s) {
+    std::vector<std::string> retval;
+    if (s == "") {
+        retval.push_back("");
+        return retval;
+    }
+    std::ostringstream ss;
+    
+    for (unsigned int i = 0; i <= s.size(); ) {
+        if (i == s.size() \
+                // edge case where s is completely empty
+                && s.size() > 0) {
+            retval.push_back(ss.str());
+            break;
+        }
+        if  (s[i] == ENCODE_ESCAPE \
+                && i+1 < s.size() \
+                && s[i+1] == ENCODE_SEPARATOR) {
+            ss << ENCODE_SEPARATOR;
+            i+=2;
+        } else if (s[i] == ENCODE_SEPARATOR) {
+            retval.push_back(ss.str());
+            ss.str("");
+            ss.clear();
+            i++;
+        } else {
+            ss << s[i];
+            i++;
+        }
     }
 
     return retval;
--- arrays_and_hashing/solution.h
+++ arrays_and_hashing/solution.h
@@ -6,8 +6,14 @@ class Solution {
         void runTests();
 
     private:
+        static const char ENCODE_SEPARATOR = ',';
+        static const char ENCODE_ESCAPE = '\\';
+
         bool hasDuplicate(std::vector<int>& nums);
         bool isAnagram(std::string s, std::string t);
         std::vector<unsigned int> twoSum(std::vector<int>& num, int target);
         std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs);
+        std::vector<int> topKFrequent(std::vector<int>& nums, int k);
+        std::string encode(std::vector<std::string>& strs);
+        std::vector<std::string> decode(std::string s);
 };
